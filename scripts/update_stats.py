cad0e1d1917ea3b8fef4d6ddb7035348f37814cd
Hasanqp <hasangubran6@gmail.com>
2026-03-09 08:58:24 +0300

    fix: limit stats scan to Beecrowd/Challenges

--- scripts/update_stats.py
+++ scripts/update_stats.py
@@ -4,7 +4,7 @@ import json
 cs_count = 0
 py_count = 0
 
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk("Beecrowd/Challenges"):
     for file in files:
         if file.endswith(".cs"):
             cs_count += 1
